b47b7b2ac463fea9101ce4743df9000355732026
marwel <marwelmancilla95@gmail.com>
2022-07-14 19:23:48 +0800

    test route

--- src/index.js
+++ src/index.js
@@ -14,8 +14,25 @@ app.use('/tasks', taskRouter)
 
 const port = process.env.PORT || 5000
 
+app.get('/tables', async(req, res) => {
+    try {
+        const psQuery1 = {
+            name: 'CreateTasksTableQuery',
+            text: 'CREATE TABLE IF NOT EXISTS tasks(taskID SERIAL,accID text,task text,status boolean,CONSTRAINT tasks_pkey PRIMARY KEY (taskID))'
+        }
+        const psQuery2 = {
+            name: 'CreateTasksTableQuery',
+            text: 'CREATE TABLE IF NOT EXISTS users(accID SERIAL,username text,password text,CONSTRAINT users_pkey PRIMARY KEY (accID))'
+        }
+    
+        await pool.query(psQuery1)
+        await pool.query(psQuery2)
 
-
+        return res.status(200).json({ msg: 'tables created'})
+    } catch (error) {
+        return res.status(500).json(error)
+    }
+})
 
 app.listen(port, async () => {
     await pool.connect()

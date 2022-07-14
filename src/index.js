0ee5569deb81dd109bd961776671e008d7af486b
marwel <marwelmancilla95@gmail.com>
2022-07-14 20:16:08 +0800

    fixed missing jwt error in env

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const cors = require('cors')
 const pool = require('./config/db')
 const accRouter = require('./routes/account')
 const taskRouter = require('./routes/tasks')
-const createDbTables = require('./config/createTables')
 
 const app = express()
 app.use(cors())
@@ -14,29 +13,7 @@ app.use('/tasks', taskRouter)
 
 const port = process.env.PORT || 5000
 
-// app.get('/tables', async(req, res) => {
-//     try {
-//         const psQuery1 = {
-//             name: 'CreateTasksTableQuery',
-//             text: 'CREATE TABLE IF NOT EXISTS tasks(taskID SERIAL,accID text,task text,status boolean,CONSTRAINT tasks_pkey PRIMARY KEY (taskID))'
-//         }
-//         const psQuery2 = {
-//             name: 'CreateUsersTableQuery',
-//             text: 'CREATE TABLE IF NOT EXISTS users(accID SERIAL,username text,password text,CONSTRAINT users_pkey PRIMARY KEY (accID))'
-//         }
-    
-//         await pool.query(psQuery1)
-//         await pool.query(psQuery2)
-
-//         return res.status(200).json({ msg: 'tables created'})
-//     } catch (error) {
-//         console.log(error)
-//         return res.status(500).json(error)
-//     }
-// })
-
 app.listen(port, async () => {
     await pool.connect()
-    // await createDbTables()
     console.log('connect successful')
 })
